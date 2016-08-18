fa8ac1c84192aaa3cc758324c9fd12733cd6bbcb
Frederick Michielssen <fmichielssen@openanalytics.eu>
2016-08-18 15:29:30 +0200

    Added logging for user authentication events

--- src/main/java/eu/openanalytics/services/UserService.java
+++ src/main/java/eu/openanalytics/services/UserService.java
@@ -2,15 +2,23 @@ package eu.openanalytics.services;
 
 import javax.inject.Inject;
 
+import org.apache.log4j.Logger;
+import org.springframework.context.ApplicationListener;
 import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.event.AbstractAuthenticationEvent;
+import org.springframework.security.authentication.event.AbstractAuthenticationFailureEvent;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService implements ApplicationListener<AbstractAuthenticationEvent> {
+
+	private Logger log = Logger.getLogger(UserService.class);
 
 	@Inject
 	Environment environment;
-	
+
 	public String[] getAdminRoles() {
 		String[] adminGroups = environment.getProperty("shiny.proxy.ldap.admin-groups", String[].class);
 		if (adminGroups == null) adminGroups = new String[0];
@@ -19,4 +27,16 @@ public class UserService {
 		}
 		return adminGroups;
 	}
+
+	@Override
+	public void onApplicationEvent(AbstractAuthenticationEvent event) {
+		Authentication source = event.getAuthentication();
+		if (event instanceof AbstractAuthenticationFailureEvent) {
+			Exception e = ((AbstractAuthenticationFailureEvent) event).getException();
+			log.info(String.format("Authentication failure [user: %s] [error: %s]", source.getName(), e.getMessage()));
+		} else if (event instanceof AuthenticationSuccessEvent) {
+			log.info(String.format("User logged in [user: %s]", source.getName()));
+		}
+	}
+
 }

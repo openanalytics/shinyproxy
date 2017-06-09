c508bb53850c03551489a97403156228ebabf73a
mnazarov <maxim.nazarov@openanalytics.eu>
2017-06-09 15:31:15 +0200

    another try with settings

--- build-shinyproxy-rpm-centos7.2.sh
+++ build-shinyproxy-rpm-centos7.2.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")            
-/opt/apache-maven-3.5.0/bin/mvn -U clean install
+/opt/apache-maven-3.5.0/bin/mvn -s settings.xml -U clean install

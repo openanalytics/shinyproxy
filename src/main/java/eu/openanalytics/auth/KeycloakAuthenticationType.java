c58f0121f5a03dee3034d714620d93dcd6a99928
Jonas Van Malder <jonas.vanmalder@openanalytics.eu>
2017-07-25 12:10:04 +0200

    added license & license header + build check

--- src/main/java/eu/openanalytics/auth/KeycloakAuthenticationType.java
+++ src/main/java/eu/openanalytics/auth/KeycloakAuthenticationType.java
@@ -1,3 +1,23 @@
+/**
+ * RDepot
+ *
+ * Copyright (C) 2012-${year} ${company}
+ *
+ * ===========================================================================
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Apache License as published by
+ * The Apache Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Apache License for more details.
+ *
+ * You should have received a copy of the Apache License
+ * along with this program.  If not, see <http://www.apache.org/licenses/>
+ */
 package eu.openanalytics.auth;
 
 import java.io.Serializable;

7c284695a653d7debaaab4259e6e2d549b0ec537
Lee Bousfield <ljbousfield@gmail.com>
2017-11-09 13:33:29 -0700

    Add script to manually generate tag override

--- gen_tag_override.py
+++ gen_tag_override.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python
-""" Manually generate a tag override hash """
-import base64
-import hashlib
-import sys
+#
+# ShinyProxy
+#
+# Copyright (C) 2016-2017 Open Analytics
+#
+# ===========================================================================
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the Apache License as published by
+# The Apache Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# Apache License for more details.
+#
+# You should have received a copy of the Apache License
+# along with this program.  If not, see <http://www.apache.org/licenses/>
+#
 
 assert sys.version_info >= (3, 2)
 

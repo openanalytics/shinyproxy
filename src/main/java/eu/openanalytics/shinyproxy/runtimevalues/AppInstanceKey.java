c22fb0d7d1c3020865a7f2f1e935440674a46a1d
Robert Koch <robekoc@amazon.com>
2023-03-21 15:19:50 +1100

    adds tags as values source

--- src/main/java/eu/openanalytics/shinyproxy/runtimevalues/AppInstanceKey.java
+++ src/main/java/eu/openanalytics/shinyproxy/runtimevalues/AppInstanceKey.java
@@ -27,8 +27,10 @@ public class AppInstanceKey extends RuntimeValueKey<String>  {
     public AppInstanceKey() {
         super("openanalytics.eu/sp-app-instance",
                 "SHINYPROXY_APP_INSTANCE",
+                "openanalytics.eu/sp-app-instance",
                 false,
                 true, // include as annotation so that the value can be recovered
+                true,
                 false,
                 true,
                 true,

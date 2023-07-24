993e127049e1549a5a756a6be8250de289c1d377
Tobia De Koninck <tobia.dekoninck@openanalytics.eu>
2023-07-24 16:00:18 +0200

    Fix refresh-openid when using context-path

--- src/main/resources/static/js/shiny.connections.js
+++ src/main/resources/static/js/shiny.connections.js
@@ -79,7 +79,7 @@ Shiny.connections = {
                 console.log("no openid refresh");
                 return;
             }
-            $.post(Shiny.api.buildURL("/refresh-openid"));
+            $.post(Shiny.api.buildURL("refresh-openid"));
         }, Shiny.app.staticState.openIdRefreshRate);
     },
 

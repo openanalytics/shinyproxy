0adc5a527355eaf4d6f9f8cbfcb98407ff30f048
Tobia De Koninck <tobia.dekoninck@openanalytics.eu>
2024-03-04 15:05:57 +0100

    Fix #32533: support apps that link to other url

---
+++ src/main/java/eu/openanalytics/shinyproxy/external/ExternalAppSpecExtensionProvider.java
@@ -0,0 +1,40 @@
+/**
+ * ShinyProxy
+ *
+ * Copyright (C) 2016-2024 Open Analytics
+ *
+ * ===========================================================================
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Apache License as published by
+ * The Apache Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Apache License for more details.
+ *
+ * You should have received a copy of the Apache License
+ * along with this program.  If not, see <http://www.apache.org/licenses/>
+ */
+package eu.openanalytics.shinyproxy.external;
+
+
+import eu.openanalytics.containerproxy.spec.ISpecExtensionProvider;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Setter
+@Getter
+@Configuration
+@ConfigurationProperties(prefix = "proxy")
+public class ExternalAppSpecExtensionProvider implements ISpecExtensionProvider<ExternalAppSpecExtension> {
+
+    private List<ExternalAppSpecExtension> specs;
+
+}

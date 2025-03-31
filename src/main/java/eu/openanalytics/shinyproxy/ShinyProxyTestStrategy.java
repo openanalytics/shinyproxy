cf8b1e376c4c3b4b2e2d87f0d0a560abb8a4a046
Tobia De Koninck <tobia.dekoninck@openanalytics.eu>
2025-03-31 11:24:15 +0200

    Fix #34863: check container health during startup to fail early

--- src/main/java/eu/openanalytics/shinyproxy/ShinyProxyTestStrategy.java
+++ src/main/java/eu/openanalytics/shinyproxy/ShinyProxyTestStrategy.java
@@ -20,6 +20,7 @@
  */
 package eu.openanalytics.shinyproxy;
 
+import eu.openanalytics.containerproxy.backend.dispatcher.ProxyDispatcherService;
 import eu.openanalytics.containerproxy.backend.strategy.IProxyTestStrategy;
 import eu.openanalytics.containerproxy.model.runtime.Proxy;
 import eu.openanalytics.containerproxy.service.StructuredLogger;
@@ -28,6 +29,7 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import java.net.HttpURLConnection;
 import java.net.URI;
@@ -43,10 +45,22 @@ import java.util.Objects;
 @Primary
 public class ShinyProxyTestStrategy implements IProxyTestStrategy {
 
-    private final StructuredLogger log = StructuredLogger.create(getClass());
+    private final StructuredLogger slog = StructuredLogger.create(getClass());
 
     @Inject
     private Environment environment;
+
+    @Inject
+    private ProxyDispatcherService proxyDispatcherService;
+
+    private int totalWaitMs;
+    private int requestTimeout;
+
+    @PostConstruct
+    public void init() {
+        totalWaitMs = Integer.parseInt(environment.getProperty("proxy.container-wait-time", "20000"));
+        requestTimeout = Integer.parseInt(environment.getProperty("proxy.container-wait-timeout", "5000"));
+    }
 
     @Override
     public boolean testProxy(Proxy proxy) {
@@ -55,46 +69,37 @@ public class ShinyProxyTestStrategy implements IProxyTestStrategy {
             return true;
         }
 
-        int totalWaitMs = Integer.parseInt(environment.getProperty("proxy.container-wait-time", "20000"));
-        int timeoutMs = Integer.parseInt(environment.getProperty("proxy.container-wait-timeout", "5000"));
-
         if (proxy.getTargets().isEmpty()) return false;
         URI targetURI = proxy.getTargets().get("");
 
         return Retrying.retry((currentAttempt, maxAttempts) -> {
-            try {
-                if (proxy.getStatus().isUnavailable()) {
-                    // proxy got stopped while loading -> no need to try to connect it since the container will already be deleted
-                    return true;
-                }
-                URL testURL = new URL(targetURI.toString() + "/");
-                HttpURLConnection connection = ((HttpURLConnection) testURL.openConnection());
-                if (currentAttempt <= 5) {
-                    // When the container has only just started (or when the k8s service has only just been created),
-                    // it could be that our traffic ends in a black hole, and we need to wait the full 5s seconds of
-                    // the timeout. Therefore, we first try a few attempts with a lower timeout. If the container is
-                    // fast, this will result in a faster startup. If the container is slow to startup, not time is waste.
-                    connection.setConnectTimeout(200);
-                    connection.setReadTimeout(200);
-                } else {
-                    connection.setConnectTimeout(timeoutMs);
-                    connection.setReadTimeout(timeoutMs);
-                }
-                connection.setInstanceFollowRedirects(false);
-                int responseCode = connection.getResponseCode();
-                if (Arrays.asList(200, 301, 302, 303, 307, 308).contains(responseCode)) {
-                    if (currentAttempt > 10) {
-                        log.info(proxy, "Container responsive");
-                    }
-                    return true;
-                }
-            } catch (Exception e) {
-                if (currentAttempt > 10) {
-                    log.warn(proxy, String.format("Container unresponsive, trying again (%d/%d): %s", currentAttempt, maxAttempts, targetURI));
-                }
+            if (proxy.getStatus().isUnavailable()) {
+                // proxy got stopped while loading -> no need to try to connect it since the container will already be deleted
+                return new Retrying.Result(false, false);
             }
-            return false;
-        }, totalWaitMs);
+            if (!proxyDispatcherService.getDispatcher(proxy.getSpecId()).isProxyHealthy(proxy)) {
+                return new Retrying.Result(false, false);
+            }
+            URL testURL = new URL(targetURI.toString() + "/");
+            HttpURLConnection connection = ((HttpURLConnection) testURL.openConnection());
+            if (currentAttempt <= 5) {
+                // When the container has only just started (or when the k8s service has only just been created),
+                // it could be that our traffic ends in a black hole, and we need to wait the full 5s seconds of
+                // the timeout. Therefore, we first try a few attempts with a lower timeout. If the container is
+                // fast, this will result in a faster startup. If the container is slow to startup, not time is wasted.
+                connection.setConnectTimeout(200);
+                connection.setReadTimeout(200);
+            } else {
+                connection.setConnectTimeout(requestTimeout);
+                connection.setReadTimeout(requestTimeout);
+            }
+            connection.setInstanceFollowRedirects(false);
+            int responseCode = connection.getResponseCode();
+            if (Arrays.asList(200, 301, 302, 303, 307, 308).contains(responseCode)) {
+                return Retrying.SUCCESS;
+            }
+            return Retrying.FAILURE;
+        }, totalWaitMs, "Checking application reachable at " + targetURI, 10, proxy, slog);
     }
 
 }
